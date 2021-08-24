48675b1333d119c2c9224e9a83e84910fb351bc9
Febima Hermawan <febimahermawan16@gmail.com>
2021-08-24 10:26:08 +0700

    Update Base - 24 August 2021

---
+++ src/lib/Routers.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+// Default
+import Sidebar from '../default/Sidebar';
+import Footer from '../default/Footer';
+
+// Pages
+// Dashboard
+import Dashboard from '../pages/dashboard/Dashboard';
+
+// User Interfaces
+// Typhography
+import Typographys from '../pages/user-interfaces/typography/Typographys';
+
+// Cards
+import Cards from '../pages/user-interfaces/cards/Cards';
+
+export class Routers extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                
+                    {/* 
+                    * ========== Main ===========
+                    */}
+                    {/* Dashboard */}
+                    <Route exact path="/">
+                        <Sidebar />
+                        <div className="app-content">
+                        <div className="content">
+                            <div className="row">
+                            <Dashboard />
+                            </div>
+                        </div>
+                        <Footer />
+                        </div>
+                    </Route>
+
+                    {/* Introduction */}
+
+
+                    {/* Changelogs */}
+
+                    {/* 
+                    * ========== User Interfaces ===========
+                    */}
+                    {/* Typography */}
+                    <Route path="/typography">
+                        <Sidebar />
+                        <div className="app-content">
+                        <div className="content">
+                            <div className="row">
+                            <Typographys />
+                            </div>
+                        </div>
+                        <Footer />
+                        </div>
+                    </Route>
+                    
+                    {/* Cards */}
+                    <Route path="/cards">
+                        <Sidebar />
+                        <div className="app-content">
+                        <div className="content">
+                            <div className="row">
+                            <Cards />
+                            </div>
+                        </div>
+                        <Footer />
+                        </div>
+                    </Route>
+
+
+                    {/* 
+                    * ========== Forms & Tables ===========
+                    */}
+                    {/* Forms */}
+
+
+                    {/* 
+                    * ========== Charts ===========
+                    */}
+                    {/* Apex */}
+
+
+                    {/* 
+                    * ========== Pages ===========
+                    */}
+                    {/* Login */}
+                </Switch>
+            </Router>
+        )
+    }
+}
+
+export default Routers
---
+++ src/pages/user-interfaces/cards/components/defaults.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class defaults extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="col-md-6">
+                    <div className="card card-light">
+                        <div className='card-header'>
+                            <div className="card-title">Cards</div>
+                        </div>
+                        <div className='card-body'>
+                            <p className="card-text">
+                                With supporting text below as a natural lead-in to additional content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default defaults
